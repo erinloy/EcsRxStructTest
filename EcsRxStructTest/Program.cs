fc5761fea9929da6a6d7851d23bc2560fc8e3aa2
Erin Loy <eloy@crazydogventures.com>
2019-12-31 12:27:38 -0800

    Add project files.

---
+++ EcsRxStructTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EcsRxStructTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
